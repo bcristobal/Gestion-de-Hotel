0085490bbcb0e2d9de0c4a0256aa5ec0be684f3e
Benat <b.cristobal@opendeusto.es>
2024-04-09 18:14:04 +0200

    Add seed data for rooms

---
+++ sql/seed-rooms.sql
@@ -0,0 +1,7 @@
+-- Insertar datos de ejemplo
+INSERT INTO room VALUES (100, 2, 'Habitación para dos personas', 33.33, 'Doble');
+INSERT INTO room VALUES (101, 1, 'Habitación individual', 25.00, 'Individual');
+INSERT INTO room VALUES (102, 3, 'Suite familiar', 70.50, 'Suite con capacidad para una familia');
+INSERT INTO room VALUES (103, 4, 'Habitación familiar', 55.75, 'Ideal para familias numerosas');
+INSERT INTO room VALUES (104, 2, 'Habitación doble deluxe', 45.99, 'Habitación con comodidades adicionales');
+INSERT INTO room VALUES (105, 1, 'Habitación individual con vistas', 30.00, 'Habitación individual con vistas al mar');
